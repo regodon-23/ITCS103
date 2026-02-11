2fdee28078c92ae45b05dcd83abbfd96561827c1
regodon-23 <regodongianandrei23@gmail.com>
2026-02-11 17:08:18 +0800

    Add files via upload

---
+++ REGODON_HO2.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+#create main window
+window = tk.Tk()
+window.title("My Profile")
+window.geometry("600x600")
+window.resizable(False, True)
+ 
+title_Label = tk.Label(window,text='My Profile',font=('Serif',30,'bold'),fg="green",bg='blue',anchor='center')
+title_Label.pack(padx=10,pady=10)
+
+tk.Label(window,text="Name: Gian Andrei Regodon",font='20',anchor="w").pack(fill="x",padx=20,pady=5)
+tk.Label(window,text="Age: 18",font='20',anchor="w").pack(fill="x",padx=20,pady=5)
+tk.Label(window,text="Course: BSIT-1A",font='20',anchor="w").pack(fill="x",padx=20,pady=5)
+tk.Label(window,text="Birthday: February 23, 2007",font='20',anchor="w").pack(fill="x",padx=20,pady=5)
+tk.Label(window,text="Motto: Be happy for what you have while working for what you want",font='30',anchor="w").pack(fill="x",padx=20,pady=5)
+
+window.mainloop()
